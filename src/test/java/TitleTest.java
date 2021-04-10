4227b4369e5874edbcde948e6a867eef6abc8e48
Andrew M <bionicmaan@gmail.com>
2021-04-10 08:59:01 +0300

    Рефакторинг кода по принципу DRY

--- src/test/java/TitleTest.java
+++ src/test/java/TitleTest.java
@@ -1,7 +1,7 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.aeonbits.owner.ConfigFactory;
-import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
@@ -13,6 +13,7 @@ public class TitleTest {
 
     private Logger logger = LogManager.getLogger(TitleTest.class);
     protected static WebDriver driver;
+    TestsData testdata = ConfigFactory.create(TestsData.class);
 
     @Before
     public void Start(){
@@ -28,33 +29,30 @@ public class TitleTest {
         logger.info("Web Driver закрыт");
     }
 
-    @Test
-    public void CheckTitle(){
+    public void CheckTitle(String expected){
         driver.get("https://otus.ru");
         logger.info("Переход по адресу https://otus.ru");
 
-        TestsData expected = ConfigFactory.create(TestsData.class);
+//        TestsData testdata = ConfigFactory.create(TestsData.class);
         logger.info("Подготовка тестовых данных");
 
         logger.info("Старт теста");
         String actual = driver.getTitle();
-        Assert.assertTrue(actual.contains(expected.mainTitle()));
-//        Assert.assertEquals("Онлайн‑курсы для профессионалов, дистанционное обучение современным профессиям",actual);
+//        String expected = testdata.mainTitle();
+        if (expected == null)
+            logger.error("NullPointerException");
+        Assert.assertTrue(actual.contains(expected));
         logger.info("Тест завершен");
     }
 
     @Test
+    public void CheckMainTitle(){
+        CheckTitle(testdata.mainTitle());
+    }
+
+    @Test
     public void CheckSpecialTitle(){
-        driver.get("https://otus.ru");
-        logger.info("Переход по адресу https://otus.ru");
-
-        TestsData expected = ConfigFactory.create(TestsData.class);
-        logger.info("Подготовка тестовых данных");
-
-        logger.info("Старт теста");
-        String actual = driver.getTitle();
-        Assert.assertTrue(actual.contains(expected.celebratoryTitle()));
-        logger.info("Тест завершен");
+        CheckTitle(testdata.celebratoryTitle());
     }
 
 }
