b52d5cf75c55bfeecfeccbbefb7885bd7fd7fb04
Andrew M <bionicmaan@gmail.com>
2021-04-14 11:37:54 +0300

    Исправлен дефект NullPointerException при работе owner

--- src/main/java/TestsData.java
+++ src/main/java/TestsData.java
@@ -1,5 +1,6 @@
 import org.aeonbits.owner.Config;
 
+@Config.Sources({"file:src/main/resources/TestsData.properties"})
 public interface TestsData extends Config {
     String mainTitle();
     String celebratoryTitle();
