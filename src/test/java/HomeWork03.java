0d67b85f4bc2df2f936383e76ffd23bfb468dd33
Andrew M <bionicmaan@gmail.com>
2021-05-12 11:35:22 +0300

    Yandex тест дописан до конца. Борьба с капчей не удалась, тест проходит по ночам, когда капча не выбрасывается.

--- src/test/java/HomeWork03.java
+++ src/test/java/HomeWork03.java
@@ -2,6 +2,8 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.aeonbits.owner.ConfigFactory;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.assertj.core.api.SoftAssertions;
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
@@ -12,28 +14,30 @@ import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.util.List;
+
 public class HomeWork03 {
     protected static WebDriver driver;
     private Logger log = LogManager.getLogger(HomeWork03.class);
     TestsData testsData = ConfigFactory.create(TestsData.class);
+    SoftAssertions softAssert = new SoftAssertions();
 
     @Before
     public void startUp(){
         WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
         log.info("Web Driver поднят");
-//        Настрока не явных ожиданий с таймаутом 3 секунды
-//        driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
     }
 
-//    @After
+    @After
     public void shutDown(){
         if (driver!=null)
             driver.quit();
+        log.info("Web Driver закрыт");
     }
 
     @Test
-    public void yandexTest() {
+    public void yandexTest(){
         driver.manage().window().maximize();
         log.info("Разворачиваем окно на весь экран");
 
@@ -44,11 +48,10 @@ public class HomeWork03 {
         driver.findElement(elctronicLink).click();
         log.info("Переход в раздел Электроника");
 
-//      TODO для обхода капчи добавить ожидание на загрузку страницы
-
+//      Попытка бороться с капчей
         String pageTitle = driver.getTitle();
         if(pageTitle == "Ой!"){
-            By captchaCheckBox = By.xpath("//span[text()='Я не робот']");
+            By captchaCheckBox = By.xpath("//span[text()='Нажмите, чтобы продолжить']");
             driver.findElement(captchaCheckBox).click();
             log.info("Проходим капчу");
         }
@@ -68,34 +71,64 @@ public class HomeWork03 {
         log.info("Сортировка по цене от меньшей к большей");
         waitForResult();
 
-        String listNameModel = addToCompare(testsData.smartphone01());
-        log.debug(listNameModel);
+        String model1FromList = addToCompare(testsData.smartphone01());
+        checkItemToCompare(model1FromList);
 
-//      TODO проверить что текст во всплывашке содержится
-//       в тексте из списка
-//        Assert.assertTrue();
+        String model2FromList = addToCompare(testsData.smartphone02());
+        checkItemToCompare(model2FromList);
+
+        driver.get("https://market.yandex.ru/my/compare-lists");
+        log.info("Переход на страницу сравнения товаров");
+
+        List<WebElement> items = driver.findElements(By.xpath("//a[contains(text(),'Смартфон')]"));
+        softAssert.assertThat(items.size()).isEqualTo(2);
+        softAssert.assertAll();
+        log.info("Проверка количества товаров на сравнении: " + items.size());
     }
 
-    public void waitForResult(){
+    private void waitForResult(){
         By spinnerLocator = By.xpath("//div[@data-tid='8bc8e36b']");
         WebElement spinner = driver.findElement(spinnerLocator);
         new WebDriverWait(driver,8).until(ExpectedConditions.invisibilityOf(spinner));
         log.info("Ожидание результата фильтрации");
     }
 
-    public void chooseBrand(String brand){
+    private void chooseBrand(String brand){
         By checkBox = By.xpath("//input[@name='Производитель " + brand + "']/parent::label");
         driver.findElement(checkBox).click();
         log.info("Выбор производителя " + brand);
     }
 
-    public String addToCompare(String item){
+    private String addToCompare(String item){
         Actions action = new Actions(driver);
+        //Находим первый в списке нужный товар
         WebElement phone = driver.findElement(By.xpath("//span[contains(text(),'" + item + "')]"));
-        String phoneName = phone.getText();
+        //Запоминаем название товара без указания цвета
+        String phoneName = phone.getText().split(",")[0];
+        //Наводим мышь на товар
         action.moveToElement(phone).build().perform();
+        //Нажимаем на кнопку сравить
         driver.findElement(By.xpath("//span[contains(text(),'" + item + "')]/ancestor::*[5]/descendant::*[5]")).click();
         log.info("Добавление к сравнению первого " + item);
+        //Возвращаем название товара
         return phoneName;
+    }
+
+    private void checkItemToCompare(String modelName){
+        By textToCompareLocator = By.xpath("//div[contains(text(),'добавлен к сравнению')]");
+        //Берем с плашки текст фактического результата
+        String actualResult = getElement(textToCompareLocator).getText();
+        //Генерируем текст ожидаемого результата
+        String expectedResult = "Товар " + modelName + " добавлен к сравнению";
+        //Делаем Soft Assert
+        softAssert.assertThat(actualResult).isEqualTo(expectedResult);
+        log.info("Проверка добавленного товара на всплывашке: " + actualResult);
+    }
+
+    private WebElement getElement(By locator){
+        return new WebDriverWait(driver,4)
+                //Каждый раз при вызове метода отрабатывает явное ожидание в 4 секунды
+                //Метод until возвращает объект Web Element
+                .until(ExpectedConditions.visibilityOfElementLocated(locator));
     }
 }
